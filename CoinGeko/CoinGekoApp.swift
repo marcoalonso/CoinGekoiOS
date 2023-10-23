d1ea15781e02c804f5e8e67beb8af293f0057fb4
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-10-23 11:16:50 -0600

    Getting Started

---
+++ CoinGeko/CoinGekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoApp.swift
+//  CoinGeko
+//
+//  Created by Marco Alonso Rodriguez on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
